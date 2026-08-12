153dab28eb97721a3de68f3e1dd1a4e05ea0648d
gabs0808 <bielzinxpwar@gmail.com>
2026-08-12 02:50:28 -0300

    Execução de exercicios em C

---
+++ Estudos-Autonomos/ex02.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(){
+    // cada codigo finalizado com ";" é uma sentença
+    int a = 1;
+    int b = 2;
+    int c = a + b;
+    printf("%d", c);
+}
---
+++ Estudos-Autonomos/ex03-tipos-de-variaveis.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void main(){
+    // caracteres
+    char nome_da_variavel = 'a'; // -127 a 127
+    unsigned charnome_da_variavel1 = 'a'; // 0 a 256
+
+    unsigned // nao deixa atribuir valor negativo a variavel e pode ser usado no char e int
+
+    // numeros inteiros
+    short int nome_da_variavel2;
+    int nome_da_variavel3;
+    long int nome_da_variavel4;
+
+    // numeros reais
+    float nome_da_variavel5;
+    double nome_da_variavel6;
+    long double nome_da_variavel7;
+
+    // constante nomeada
+    const int MAX = 100; // variavel constante é algo que nao podera ser auterado no decorrer do codigo, gerando erro
+}
