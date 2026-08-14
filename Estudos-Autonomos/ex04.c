10b90e0147d1f06611b587eaeb5a2f4787ec9598
gabs0808 <bielzinxpwar@gmail.com>
2026-08-14 14:47:25 -0300

    atualização
    novas atividades

--- Estudos-Autonomos/ex04.c
+++ Estudos-Autonomos/ex04.c
@@ -3,20 +3,34 @@
 void main(){
     // caracteres
     char nome_da_variavel = 'a'; // -127 a 127
-    unsigned charnome_da_variavel1 = 'a'; // 0 a 256
+    unsigned char nome_da_variavel1 = 'a'; // 0 a 256
 
+    // imprimir
+    printf("%c\n", nome_da_variavel1);
     unsigned // nao deixa atribuir valor negativo a variavel e pode ser usado no char e int
 
     // numeros inteiros
-    short int nome_da_variavel2;
-    int nome_da_variavel3;
-    long int nome_da_variavel4;
+    short int nome_da_variavel2 = 1;
+    int nome_da_variavel3 = 2;
+    long int nome_da_variavel4 = 3;
+
+    // imprimir
+    printf("%i\n", nome_da_variavel2);
+    printf("%i\n", nome_da_variavel3);
+    printf("%li\n", nome_da_variavel4);
 
     // numeros reais
-    float nome_da_variavel5;
-    double nome_da_variavel6;
-    long double nome_da_variavel7;
+    float nome_da_variavel5 = 1.1f;
+    double nome_da_variavel6 = 1.2;
+    long double nome_da_variavel7 = 3.9e-23L;
+
+    // imprimir
+    printf("%f\n", nome_da_variavel5);
+    printf("%f\n", nome_da_variavel6);
+    printf("%e\n", nome_da_variavel7);
 
     // constante nomeada
     const int MAX = 100; // variavel constante é algo que nao podera ser auterado no decorrer do codigo, gerando erro
+
+    printf("%d\n", MAX);
 }
---
+++ Estudos-Autonomos/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main(){
+    // truncamento, apaga o numero depois da virgula, nao arredonda
+
+   float b = 3.9;
+   int i = (int)b;
+   printf("%i\n", i);
+
+    // ou direto na printagem
+    
+    int c = 3;
+    printf("%f", (float)c);
+}
